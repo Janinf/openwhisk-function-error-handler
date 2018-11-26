5e7eb677fb727d5e5e837c8c310824c7c5ad3d0b
Jan Armbruster <armbruster.dev@gmail.com>
2018-11-26 11:03:45 +0100

    Added error builder

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
-async function handleError(params, action, errorHandler = err => ({err: {status: 500, message: err.message}})) {
-    if (params.err) return errorHandler(params.err)
+/**
+ * Represents an HTTP error message
+ */
+class HttpError {
+    /**
+     * 
+     * @param {number} status HTTP status code
+     * @param {string} message Error message
+     */
+    constructor(status, message) {
+        this.status = status
+        this.message = message
+    }
+}
+
+/**
+ * 
+ * @param {number} status HTTP status code
+ * @param {string} message Error message
+ * @returns {{HttpError}} Error object
+ */
+function buildError(status, message) {
+    return {err: new HttpError(status, message)}
+}
+
+/**
+ * Wraps a openwhisk serverless framework action in a standard changeable error handler
+ * @param {*} params Action input parameters
+ * @param {(function(*): *)} action Action code, takes parameters
+ * @returns {* | {HttpError}} Result of action code or error object
+ */
+async function handleError(params, action) {
+    if (params.err) return buildError(params.err.status, params.err.message)
 
     try {
         return await action(params)
     } catch(e) {
         console.error(e)
-        return errorHandler(e)
+        return buildError(500, e.message)
     }
 }
 
-module.exports = handleError
+module.exports = {buildError, handleError}
